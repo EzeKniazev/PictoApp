90bbc6420be6ec9d33927ddd4423c159024efa9c
EzeKniazev <ezekniazev@gmail.com>
2022-11-06 18:22:55 -0300

    Damasco

--- PictoApp/CategoryElements/DeportesPage.xaml.cs
+++ PictoApp/CategoryElements/DeportesPage.xaml.cs
@@ -41,5 +41,49 @@ namespace PictoApp.CategoryElements
         {
             await TextToSpeech.SpeakAsync("Ciclismo");
         }
+        private async void BtnHockey_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Hockey");
+        }
+        private async void BtnBoxeo_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Boxeo");
+        }
+        private async void BtnPadel_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Padel");
+        }
+        private async void BtnHandbol_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Handbol");
+        }
+        private async void BtnSurf_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Surf");
+        }
+        private async void BtnGolf_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Golf");
+        }
+        private async void BtnPingPong_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Ping-Pong");
+        }
+        private async void BtnAjedrez_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Ajedrez");
+        }
+        private async void BtnCorrer_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Correr");
+        }
+        private async void BtnVoleibol_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Voleibol");
+        }
+        private async void BtnBocha_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Bocha");
+        }
     }
 }
