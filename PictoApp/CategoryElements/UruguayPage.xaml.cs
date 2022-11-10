0fb21cc2a8d3d744fab7e042b0a06416c5ced387
EzeKniazev <ezekniazev@gmail.com>
2022-11-10 08:10:50 -0300

    Nombres

--- PictoApp/CategoryElements/UruguayPage.xaml.cs
+++ PictoApp/CategoryElements/UruguayPage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace PictoApp.CategoryElements
+namespace PictogrApp.CategoryElements
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UruguayPage : ContentPage
